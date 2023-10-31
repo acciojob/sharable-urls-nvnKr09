2df9418ebd95b723d9812296e4db6397ff2c79ae
Naveen Kumar <134830093+nvnKr09@users.noreply.github.com>
2023-10-31 19:27:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+const form = document.getElementById("urlForm");
+        const urlElement = document.getElementById("url");
+
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
+
+            // Build the URL with a query string
+            const urlString = `https://localhost:8080/?name=${name}&year=${year}`;
+
+            // Update the h3 element with the generated URL
+            urlElement.textContent = urlString;
+        });
